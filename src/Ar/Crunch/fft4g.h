6db1009cbfe7422a880006d93bbd9c7979a55851
Jim Neumann <jim.a.neumann@gmail.com>
2023-11-09 14:01:23 -0600

    Fix return type lost when header was replaced

--- src/Ar/Crunch/fft4g.h
+++ src/Ar/Crunch/fft4g.h
@@ -7,7 +7,7 @@
  * 
  */
 
- cdft(int, int, double *, int *, double *);
+void cdft(int, int, double *, int *, double *);
 void rdft(int, int, double *, int *, double *);
 void ddct(int, int, double *, int *, double *);
 void ddst(int, int, double *, int *, double *);
